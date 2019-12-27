8578c7210f97eea9f33b2925e1d93aec6c97a218
Dimitri Gedda <gratitudeadore@fastmail.com>
2019-12-27 03:28:06 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,9 +6,9 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(name)
+  def back_project(project)
     
-    backed_projects << name
+    backed_projects << project
     
   end
 end
